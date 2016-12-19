86ff87929dd293c6150586b7fc72e27919035f28
asarpi <noxnoctis35@gmail.com>
2016-12-19 23:16:12 +0100

    create a parser for RGB codes. the bytecode is the next:  < 1st LED ID; 2nd LED ID 1st R; 1st G; 1st B; 2nd R; 2nd G; 2nd B  > the indicator before send the stream a sended byte with 255 value in the first place. The parser stops in the arduino, if an index is higher than maximum number of LEDs

--- LedStrip_interface.h
+++ LedStrip_interface.h
@@ -16,14 +16,19 @@
 #define STRIP_PIN 6
 #define MSG_PARSER_COUNTER_LIMIT 1000
 
+
+
 class LedStrip_interface : public Adafruit_NeoPixel
 {
 private: 
-	uint16_t RGB_stack[NUM_OF_LEDS][3];
-	uint16_t t_RGB_stack[NUM_OF_LEDS][3];
+	uint16_t RGB_stack[NUM_OF_LEDS][3]; // NUM_OF_LEDS x 3 matrix for storing RGB values for each LED on the strip
+	uint16_t t_RGB_stack[NUM_OF_LEDS][3]; // temp matrix with same size with original stack
 	String msg;
 	long int cyc_parser_msg_wait_counter = 0;
+
+	bool flag_waiting_for_strip_codes;
 	
+	// function for reseting all elements of the RGB stack.
 	void init_RGB_stack() {
 		for (uint16_t led_num = 0; led_num < NUM_OF_LEDS; led_num++) {
 			for (uint8_t color_num = 0; color_num < 3; color_num++) {
@@ -32,12 +37,15 @@ private:
 		}
 	}
 
+	// function for testing each LED on the strip
 	void strip_test() {
-		
+		// reset all LED
 		for (uint16_t led_id = 0; led_id < numPixels() + 15; led_id++) {
 			setPixelColor(led_id - 10, Color(0, 0, 0));
 		}
 		show();
+		
+		// RGB "sneak"
 		for (uint16_t led_id = 0; led_id < numPixels() + 15; led_id++) {
 			delay(20);
 			//R
@@ -64,14 +72,16 @@ private:
 			show();
 		}
 		delay(20);
+
+		// RGB sneak's going out  (shifting the stack, and fill with zeros the new elements)
 		for (uint16_t led_id = 0; led_id < numPixels(); led_id++) {
 			delay(20);
 			shift_RGB_stack(1, 0, 0, 0);
 			show_full_RGB_stack();
 		}
-		
-		
-	}
+	} //end strip test
+
+	// send a selected LED's RGB values to Serial port
 	void print_led_RGB(uint16_t px) {
 		Serial.print("l: "); Serial.print(int(px));
 		Serial.print("  R:  ");  Serial.print(int(RGB_stack[px][0]));
@@ -79,45 +89,31 @@ private:
 		Serial.print("  B:  ");  Serial.print(int(RGB_stack[px][2]));
 		Serial.println("");
 	}
-	/*
-	void shift_RGB_stack(uint16_t steps, uint16_t new_R, uint16_t new_G, uint16_t new_B) {
-		//shifting
-		for (uint16_t led_id = steps; led_id < numPixels(); led_id++) {
-			t_RGB_stack[led_id][0] = RGB_stack[led_id - steps][0];
-			t_RGB_stack[led_id][1] = RGB_stack[led_id - steps][1];
-			t_RGB_stack[led_id][2] = RGB_stack[led_id - steps][2];
-		}
-		//fill empty space
-		for (uint16_t led_id = 0; led_id < steps+1; led_id++) {
-			t_RGB_stack[led_id][0] = 0;
-			t_RGB_stack[led_id][1] = 0;
-			t_RGB_stack[led_id][2] = 0;
-		}
-		//store new values
-		for (uint16_t led_id = 0; led_id < numPixels(); led_id++) {
-			for (uint16_t c = 0; c < 3; c++) {
-				RGB_stack[led_id][c] = t_RGB_stack[led_id][c];
-			}
-		}
-	}
-	*/
 	
 public:
+	// init of Base interface class
 	LedStrip_interface() : Adafruit_NeoPixel(NUM_OF_LEDS, STRIP_PIN, NEO_GRB + NEO_KHZ800) {
 		begin();
-		
 	}
+
+	// method for initialization in the Setup() function of main ino file
 	void init() {
+		begin();
 		strip_test();
+		flag_waiting_for_strip_codes = false;
 		Serial.println("LED strip initialized");
 	}
 
+
+	// set RGB values of a specified LED in the _STACK_
 	void set_one_LED_in_stack(uint16_t LED_id, uint16_t color_R, uint16_t color_G, uint16_t color_B) {
 		RGB_stack[LED_id][0] = color_R;
 		RGB_stack[LED_id][1] = color_G;
 		RGB_stack[LED_id][2] = color_B;
 
 	}
+
+	// method for shifting each element in the stack with specified steps and filling the new elements with an RGB value
 	void shift_RGB_stack(uint16_t steps, uint16_t new_R, uint16_t new_G, uint16_t new_B) {
 		//shifting
 		for (uint16_t led_id = steps; led_id < numPixels(); led_id++) {
@@ -138,21 +134,34 @@ public:
 			}
 		}
 	}
+
+	// put the STACK to the STRIP
 	void show_full_RGB_stack() {
 		for (uint16_t led_id = 0; led_id < numPixels(); led_id++) {
 			setPixelColor(led_id, Color(RGB_stack[led_id][0], RGB_stack[led_id][1], RGB_stack[led_id][2]));
 		}
 		show();
 	}
-	void parseCmd() {
-		if (Serial.available()) {
-			msg = Serial.readString();
-			if ((msg[0] == 'S') && (msg[1] == '_')) {
-				init_RGB_stack();
-				Serial.println(msg);
-				Serial.println(msg.length());
 
-			}
+
+	// method for parsing RGB cmds for each LED on the strip
+	void parse_strip_codes(byte *received_value) {
+
+		//set LEDs' RGB codes
+		if (flag_waiting_for_strip_codes) {
+			set_one_LED_in_stack(received_value[0], received_value[2], received_value[3], received_value[4]);
+			set_one_LED_in_stack(received_value[1], received_value[5], received_value[6], received_value[7]);
+			show_full_RGB_stack();
+		}
+		// reset wait flag
+		if (received_value[0] >= NUM_OF_LEDS) {
+			setPixelColor(20, Color(50, 0, 0));
+			show();
+			flag_waiting_for_strip_codes = false;
+		}
+		// set wait flag (overrite reset)
+		if (received_value[0] == 255) {
+			flag_waiting_for_strip_codes = true;
 		}
 	}
 
--- addrLedStrip_arduino.ino
+++ addrLedStrip_arduino.ino
@@ -1,27 +1,42 @@
 #include "LedStrip_interface.h"
 
 LedStrip_interface strip = LedStrip_interface();
+byte received_value[8];
+bool flag_receive_strip_codes;
 
-String received_value;
 void setup()
 {
-
-  /* add setup code here */
-	Serial.begin(56000);
+	flag_receive_strip_codes = false;
+	Serial.begin(9600);
 	Serial.println("Hi, I am a fancy interface for addressable LED strip");
 	strip.init();
+
+	strip.set_one_LED_in_stack(15, 50, 0, 0);
+	strip.show_full_RGB_stack();
+
+	for (uint8_t i = 0; i < sizeof(received_value); i++) {
+		received_value[i] = 0;
+	}
 	
 }
 
+size_t size_of_buffer  = 8;
 void loop()
 {
-	//strip.setPixelColor(15, strip.Color(0, 0, 100));
-	//6strip.show();
+	
+
+	
 	if (Serial.available()) {
-		received_value += Serial.readString();
-		strip.set_one_LED_in_stack(15, 100, 0, 0);
-		strip.show_full_RGB_stack();
-		Serial.println(received_value);
+
+		// handling of the bytes on the Serial line
+		Serial.readBytes(received_value, size_of_buffer);
+
+		// sending the received bytes to the parser
+		strip.parse_strip_codes(received_value);
+		
+		
+		
+
 	}
 
 
